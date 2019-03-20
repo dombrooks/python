a90158c2a4c5bf155e7d52d9eb5a75bf1efe7926
dombrooks <dombrooks@hotmail.com>
2019-03-20 15:52:55 +0000

    Create PostFix.py

---
+++ PostFix.py
@@ -0,0 +1,46 @@
+debug = True
+#debug = False
+def log(s):
+    if debug:
+        print(s)
+
+def is_number(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
+
+def main(*argv):
+    postFixStack = []
+    result = 0
+    log("Input length : " + str(len(argv)))
+    log("Input: " + ' '.join(str(v) for v in argv))
+    for v in argv:
+        token = str(v)
+        log(token)
+        if is_number(v):
+            postFixStack.append(int(token))
+            log("Appended " + token)
+        elif token in ['*', '/', '+', '-', '%']:
+            #log(token +  " is not number")
+            rhs = int(postFixStack.pop())
+            lhs = int(postFixStack.pop())
+            log(str(lhs) + " " + str(v) + " " + str(rhs))
+            if token == "*":
+                result = lhs * rhs
+            elif token == "/":
+                result = lhs / rhs
+            elif token == "+":
+                result = lhs + rhs
+            elif token == "-":
+                result = lhs - rhs
+            elif token == "%":
+                result = lhs % rhs
+            postFixStack.append(result)
+        else:
+            raise Exception("Invalid symbol " + token)
+    print("Result: " + str(postFixStack.pop()))
+
+
+main(5, 6, 7, '*', '+', 1, '-')
